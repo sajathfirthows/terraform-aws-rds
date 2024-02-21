26834ab48d4dc43386095c2fd387c188b656f8c9
Sajath Firthows <106432527+sajathfirthows@users.noreply.github.com>
2024-02-21 13:27:29 +0530

    Create securitygrp.tf

---
+++ securitygrp.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "rds_sg" {
+  name        = "rds_sg"
+  description = "Security group for RDS"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
